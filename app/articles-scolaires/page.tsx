8e9838dea5d9659e6d70c14c1725c346af9db5ca
pot20pat-oss <pot20pat@gmail.com>
2026-09-04 19:08:49 -0400

    Ajouter la page Articles scolaires

---
+++ app/articles-scolaires/page.tsx
@@ -0,0 +1,2 @@
+import CategoryStorefront from "../category-storefront";
+export default function Page(){return <CategoryStorefront title="Articles scolaires" subtitle="Cartables, sacs, gourdes et fournitures pour la rentrée." categories={["scolaire","sacs"]}/>}
