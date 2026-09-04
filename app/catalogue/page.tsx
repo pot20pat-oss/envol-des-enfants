8abd6ceecc221490a9173fb8fd6f65f866c64bef
pot20pat-oss <pot20pat@gmail.com>
2026-09-04 19:08:16 -0400

    Ajouter la page Catalogue

---
+++ app/catalogue/page.tsx
@@ -0,0 +1,2 @@
+import CategoryStorefront from "../category-storefront";
+export default function Page(){return <CategoryStorefront title="Catalogue" subtitle="Tous les articles disponibles dans votre boutique."/>}
