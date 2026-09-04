3ca04f81f1d135a0d1611e820113bc9fb2f7316b
pot20pat-oss <pot20pat@gmail.com>
2026-09-04 19:08:38 -0400

    Ajouter la page Bébé et enfants

---
+++ app/bebe-enfants/page.tsx
@@ -0,0 +1,2 @@
+import CategoryStorefront from "../category-storefront";
+export default function Page(){return <CategoryStorefront title="Bébé & enfants" subtitle="Articles pour bébé, vêtements et chaussures." categories={["bebe","vetements","chaussures"]}/>}
