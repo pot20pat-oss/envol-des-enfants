30d619cc3c9200985d46484cf4f20f4f981bf12d
pot20pat-oss <pot20pat@gmail.com>
2026-09-04 19:08:26 -0400

    Ajouter la page Jouets

---
+++ app/jouets/page.tsx
@@ -0,0 +1,2 @@
+import CategoryStorefront from "../category-storefront";
+export default function Page(){return <CategoryStorefront title="Jouets" subtitle="Jouets éducatifs, véhicules, jeux d’eau, dinosaures, animaux et jeux d’imitation." categories={["eveil","vehicules","piscine","imitation","dinosaures","animaux"]}/>}
