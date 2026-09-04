51c9944a80b9da16ff8eebd99c74a13d06fbd0fb
pot20pat-oss <pot20pat@gmail.com>
2026-09-04 07:41:22 -0400

    Activer la navigation colorée

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./navigation-large.css";
+import "./nav-joy.css";
 
 export const metadata: Metadata = {
   title: "Envol des Enfants",
