9c4cd3ec35e73fc90ddf901953c4f21f2408bf0b
pot20pat-oss <pot20pat@gmail.com>
2026-09-05 19:06:42 -0400

    Affiche les nouvelles catégories jouets mama4

--- app/jouets/page.tsx
+++ app/jouets/page.tsx
@@ -1,2 +1,2 @@
 import CategoryStorefront from "../category-storefront";
-export default function Page(){return <CategoryStorefront title="Jouets" subtitle="Jouets éducatifs, véhicules, jeux d’eau, dinosaures, animaux et jeux d’imitation." categories={["eveil","vehicules","piscine","imitation","dinosaures","animaux"]}/>}
+export default function Page(){return <CategoryStorefront title="Jouets" subtitle="Jouets éducatifs, super-héros, robots, jeux sportifs, véhicules, jeux d’eau, dinosaures, animaux et jeux d’imitation." categories={["eveil","super_heros","robots","jeux_sportifs","vehicules","piscine","imitation","dinosaures","animaux"]}/>}
